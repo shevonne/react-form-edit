23917da8f371b5c886c809bd6e4178e926e87fab
“shevon” <657869423@qq.com>
2024-05-30 10:23:55 +0800

    feat:新增列表组件发布删除功能，父子组件事件

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -5,19 +5,36 @@ type PropsType = {
   id: string;
   title: string;
   isPublished: boolean;
+  deleteQuestion: (id: string) => void;
+  publishQuestion: (id: string) => void;
 };
 
 //单个问卷条目子组件
 const QuestionCard: FC<PropsType> = (props) => {
-  const { id, title, isPublished } = props;
+  const { id, title, isPublished, deleteQuestion, publishQuestion } = props;
   return (
     <div key={id} className="item-list">
       <strong>{title}</strong>
       {isPublished ? (
-        <button className="item-list-pub">已发布</button>
+        <span className="item-list-pub">已发布</span>
       ) : (
-        <button>未发布</button>
+        <span>未发布</span>
       )}
+
+      <button
+        onClick={() => {
+          deleteQuestion(id);
+        }}
+      >
+        删除问卷
+      </button>
+      <button
+        onClick={() => {
+          publishQuestion(id);
+        }}
+      >
+        发布问卷
+      </button>
     </div>
   );
 };
--- src/list.tsx
+++ src/list.tsx
@@ -7,6 +7,44 @@ const List: FC = () => {
     { id: 'req3', title: '问卷列表3', show: false },
     { id: 'req4', title: '问卷列表4', show: true },
   ]);
+
+  function add() {
+    //后三位随机数
+    let randomNumber = Math.random().toString().slice(-3);
+    setQuestionList(
+      questionList.concat({
+        id: 'req' + randomNumber,
+        title: '问卷列表' + randomNumber,
+        show: false,
+      })
+    );
+  }
+
+  //删除
+  function deleteQuestion(id: string) {
+    setQuestionList(
+      questionList.filter((q) => {
+        if (q.id === id) return false;
+        else return true;
+      })
+    );
+  }
+
+  //发布
+  function publishQuestion(id: string) {
+    setQuestionList(
+      questionList.map((q) => {
+        if (q.id !== id) return q;
+        else {
+          return {
+            ...q,
+            show: true,
+          };
+        }
+      })
+    );
+  }
+
   return (
     <div>
       <h1>问卷调查列表</h1>
@@ -15,13 +53,19 @@ const List: FC = () => {
         return (
           <div>
             <QuestionCard
+              key={id}
               id={id}
               title={title}
               isPublished={show}
+              deleteQuestion={deleteQuestion}
+              publishQuestion={publishQuestion}
             ></QuestionCard>
           </div>
         );
       })}
+      <div>
+        <button onClick={add}>新增问卷</button>
+      </div>
     </div>
   );
 };
