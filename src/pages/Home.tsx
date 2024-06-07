637d823e73046c828bfec290993de274f99a7334
Shevon Cooper <657869423@qq.com>
2024-06-07 15:34:41 +0800

    feat:完善主页样式

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,29 @@
 import React, { FC } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { HOME_PATHNAME, MANAGE_INDEX_PATHNAME } from '../router';
+import { useNavigate } from 'react-router-dom';
+import styles from './Home.module.scss';
+import { Button, Typography } from 'antd';
+
+const { Title, Paragraph } = Typography;
+
 const Home: FC = () => {
   const nav = useNavigate();
   const goToLogin = () => {
     nav({
-      pathname: '/login',
+      pathname: MANAGE_INDEX_PATHNAME,
       search: 'username=shevon',
     });
   };
   return (
-    <div>
-      <p>
-        <Link to="/manage/list">我的列表</Link>
-      </p>
+    <div className={styles.container}>
+      <Title>问卷调查 | 在线投票</Title>
+      <Paragraph>
+        已累计创建问卷1090份，发布问卷100份，收到答卷10000份
+      </Paragraph>
       <div>
-        <button onClick={goToLogin}>登录</button>
+        <Button onClick={goToLogin} type="primary" size="large">
+          开始使用
+        </Button>
       </div>
     </div>
   );
