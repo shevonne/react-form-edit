b0511e3dfe4ceddb658ca68eabb36760a22bdd3f
Shevon Cooper <657869423@qq.com>
2024-06-07 15:42:39 +0800

    style:完善404页面样式

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,22 @@
 import React, { FC } from 'react';
+import { Button, Result } from 'antd';
+import { MANAGE_INDEX_PATHNAME } from '../router';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound: FC = () => {
-  return <div>404页面</div>;
+  const nav = useNavigate();
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="抱歉，您访问的页面不存在"
+      extra={
+        <Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>
+          回到首页
+        </Button>
+      }
+    />
+  );
 };
 
 export default NotFound;
