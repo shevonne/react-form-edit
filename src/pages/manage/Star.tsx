1dbd173ceef2c98a91dccd02822ced2cbb450ff9
Shevon Cooper <657869423@qq.com>
2024-06-12 10:19:27 +0800

    feat:登录注册页面表单完善

--- src/pages/manage/Star.tsx
+++ src/pages/manage/Star.tsx
@@ -4,6 +4,7 @@ import styles from './Common.module.scss';
 import { useTitle } from 'ahooks';
 import QuestionCard from '../../components/QuestionCard';
 import { Empty, Typography } from 'antd';
+import ListSearch from '../../components/ListSearch';
 //mock数据
 const rowQuestionList = [
   {
@@ -35,7 +36,9 @@ const Star: FC = () => {
         <div className={styles.left}>
           <Title level={3}>我的问卷</Title>
         </div>
-        <div className={styles.right}>搜索</div>
+        <div className={styles.right}>
+          <ListSearch />
+        </div>
       </div>
       <div className={styles.content}>
         {questionList.length === 0 && <Empty description="暂无数据" />}
