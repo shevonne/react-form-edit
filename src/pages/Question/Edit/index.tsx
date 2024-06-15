bca6fc67711646865d70d75a850dbf7e8f459c14
Shevon Cooper <657869423@qq.com>
2024-06-15 10:43:51 +0800

    feat:新增获取问卷数据hook,完善问卷编辑和问卷统计

--- src/pages/Question/Edit/index.tsx
+++ src/pages/Question/Edit/index.tsx
@@ -1,8 +1,17 @@
 //问卷编辑
 import React, { FC } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { getQuestionService } from '../../../services/question';
+import useLoadQuestionData from '../../../Hooks/useLoadQuestionData';
 
 const Edit: FC = () => {
-  return <div>问卷编辑</div>;
+  const { loading, questionData } = useLoadQuestionData();
+  return (
+    <div>
+      <p>问卷编辑</p>
+      {loading ? <p>加载中</p> : <p>{JSON.stringify(questionData)}</p>}
+    </div>
+  );
 };
 
 export default Edit;
