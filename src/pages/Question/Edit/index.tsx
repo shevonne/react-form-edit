8e9c6b0816f6ac1fd7b09d4a95bfb2da9658813e
Shevon Cooper <657869423@qq.com>
2024-06-30 07:20:10 +0800

    feat:新增编辑页面组件

--- src/pages/Question/Edit/index.tsx
+++ src/pages/Question/Edit/index.tsx
@@ -3,13 +3,26 @@ import React, { FC } from 'react';
 // import { useParams } from 'react-router-dom';
 // import { getQuestionService } from '../../../services/question';
 import useLoadQuestionData from '../../../Hooks/useLoadQuestionData';
-
+import styles from './index.module.scss';
+import EditCanvas from './EditCanvas';
 const Edit: FC = () => {
   const { loading, data } = useLoadQuestionData();
   return (
-    <div>
-      <p>问卷编辑</p>
-      {loading ? <p>加载中</p> : <p>{JSON.stringify(data)}</p>}
+    <div className={styles.container}>
+      <div>header</div>
+      <div className={styles['content-wrapper']}>
+        <div className={styles.content}>
+          <div className={styles.left}>left</div>
+          <div className={styles.main}>
+            <div className={styles['canvas-wrapper']}>
+              <div style={{ height: '900px' }}>
+                <EditCanvas />
+              </div>
+            </div>
+          </div>
+          <div className={styles.right}>right</div>
+        </div>
+      </div>
     </div>
   );
 };
